bb50d2d917c44272050f7c60cb1ca7c7bf2eecf9
Luca Fabian <lfabian8121@stu.d214.org>
2025-04-14 10:18:57 -0500

    added name picker pro top title

--- ContentView.swift
+++ ContentView.swift
@@ -2,11 +2,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HStack {
+            Text("NAME PICKER")
+                .font(.largeTitle)
+                .bold()
+            Text("PRO")
+                .background(.red)
+                .font(.largeTitle)
+                .bold()
+                
         }
     }
 }
