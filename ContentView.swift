7e5daa341015c650a16340608c7da8e9c59482f6
nienio1 <admin@oskar-piatek.com>
2025-04-14 11:10:10 -0500

    put navigationstack

--- ContentView.swift
+++ ContentView.swift
@@ -2,39 +2,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack{
-            
-            
-            Color .cyan
-            
-            VStack{
-                HStack {
+        NavigationStack{
+            ZStack{
+                
+                
+                Color .cyan
+                
+                VStack{
+                    HStack {
+                        
+                        Text("NAME PICKER")
+                            .font(.largeTitle)
+                            .bold()
+                            .foregroundStyle(.white)
+                        Text("PRO")
+                            .foregroundStyle(.white)
+                            .font(.largeTitle)
+                            .bold()
+                        
+                        
+                    }
                     
-                    Text("NAME PICKER")
-                        .font(.largeTitle)
-                        .bold()
-                        .foregroundStyle(.white)
-                    Text("PRO")
-                        .foregroundStyle(.white)
-                        .font(.largeTitle)
-                        .bold()
                     
+                    
+                    
+                    NavigationLink(destination: PlayView()){
+                        Text("Play")
+                            .font(.title)
+                            .padding()
+                            .foregroundStyle(.white)
+                    }
+//                    .navigationTitle("Start")
                     
                 }
                 
-
-            
-               
-                NavigationLink(destination: PlayView()){
-                    Text("Play")
-                        .font(.title)
-                        .padding()
-                        .foregroundStyle(.white)
-                }
-                .navigationTitle("Start")
-
             }
-            
         }
     }
 }
