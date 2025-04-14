28a07e967dc28dd7ddfbd0fffd4e67da9721d513
nienio1 <admin@oskar-piatek.com>
2025-04-14 10:46:36 -0500

    Started working on the name picker

--- PlayView.swift
+++ PlayView.swift
@@ -1,11 +1,20 @@
 import SwiftUI
 
 struct PlayView: View {
+    @State var names: [String] = []
+    @State var name: String = ""
     var body: some View {
-        
-        VStack{
-            Text("PlayView")
-        }
-        
+        ForEach(names, id: \.self) { name in
+                        Text(name)
+                    }
+        TextField("Name", text: $name)
+            .onSubmit {
+                names.append(name)
+                name = ""
+            }
     }
+}
+
+#Preview {
+    PlayView()
 }
