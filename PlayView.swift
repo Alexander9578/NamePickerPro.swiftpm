1913801bde9fec69f8841a94040afccd410ab9cb
nienio1 <admin@oskar-piatek.com>
2025-04-14 11:08:35 -0500

    created the basic name picking function

--- PlayView.swift
+++ PlayView.swift
@@ -3,15 +3,33 @@ import SwiftUI
 struct PlayView: View {
     @State var names: [String] = []
     @State var name: String = ""
+    @State var chosenNum: Int = 0
+    @State var showName: Bool = false
     var body: some View {
-        ForEach(names, id: \.self) { name in
-                        Text(name)
-                    }
+        Text("Names:")
+        ForEach(names.indices, id: \.self) { name in
+//            Text("\(name + 1). \(names[name])")
+            Text(names[name])
+        }
         TextField("Name", text: $name)
             .onSubmit {
-                names.append(name)
-                name = ""
+                if name.isEmpty {
+                } else {
+                    names.append(name)
+                    name = ""
+                }
             }
+        Button("Choose Random Name") {
+            if names.isEmpty {
+                showName = false
+            } else {
+                showName = true
+                chosenNum = Int.random(in: 0..<names.count)
+            }
+        }
+        if showName == true {
+            Text("Chosen Name: \(names[chosenNum])")
+        }
     }
 }
 
