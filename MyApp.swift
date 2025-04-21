d1a23394329672888d9ea3be4079d7e44b3fdc8e
Alexander9578 <adiaz8099@stu.d214.org>
2025-04-21 10:45:08 -0500

    Started fixing settingsview

--- MyApp.swift
+++ MyApp.swift
@@ -1,10 +1,18 @@
 import SwiftUI
 
+class ThemeManager: ObservableObject {
+    @Published var themeColor: Color = .blue
+}
+
 @main
 struct MyApp: App {
+    @StateObject private var themeManager = ThemeManager()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                        .environmentObject(themeManager)
+            
         }
     }
 }
