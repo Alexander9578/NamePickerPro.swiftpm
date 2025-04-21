f1f1e3e9a53250fd5d3b7805730595d625b02ec4
Ryusei <ryamada8526@stu.d214.org>
2025-04-21 10:31:26 -0500

    good code

--- SettingsView.swift
+++ SettingsView.swift
@@ -1,14 +1,9 @@
 import SwiftUI
 
 struct SettingsView: View {
-<<<<<<< Updated upstream
     @State var bgColor =
     Color(.sRGB, red: 135/255, green: 206/255, blue: 235/255 )
-=======
-    @State private var bgColor =
-    Color(.sRGB, red: 0.0, green: 0.7, blue: 1.5)
-
->>>>>>> Stashed changes
+   
     var body: some View {
         ZStack{
             if #available(iOS 17.0, *) {
