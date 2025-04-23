2095a686633f1780fddaaea28591b8fe31c9aff6
Luca Fabian <lfabian8121@stu.d214.org>
2025-04-23 10:48:56 -0500

    fixed ui

--- SettingsView.swift
+++ SettingsView.swift
@@ -8,43 +8,34 @@ struct SettingsView: View {
                 .ignoresSafeArea()
             VStack{
                 ZStack{
-                    RoundedRectangle(cornerRadius: 10)
-                        .foregroundStyle(.red)
-                        .frame(width: 220, height: 50)
-                        .offset(y:-150)
+                    RoundedRectangle(cornerRadius: 35)
+                        .frame(width: 330, height: 250)
+                        .foregroundStyle(.white)
                     Text("App Settings")
                         .font(.largeTitle)
                         .bold()
                         .foregroundStyle(.white)
                         .offset(y:-150)
-                }
                     
-                ZStack{
-                    RoundedRectangle(cornerRadius: 10)
-                        .foregroundStyle(.red)
-                        .frame(width: 360, height: 50)
-                        .offset(y:-10)
-                    
-                    Text("Change Background Color")
-                        .bold()
-                        .foregroundStyle(.white)
-                        .font(.title)
-                        .offset(y:-10)
+                    VStack{
+                        
+                        Text("Change background")
+                            .offset(y: -50)
+                            .foregroundStyle(.black)
+                            .bold()
+                            
+                        
+                        ColorPicker("Color", selection: $bgColor)
+                                .bold()
+                                .foregroundStyle(.white)
+                                .font(.title)
+                                .offset(y: -50)
+                                .frame(width: 50 , height: 50)
+                                
+                        }
+                    }
                 }
-                
-                ZStack{
-                    RoundedRectangle(cornerRadius: 10)
-                        .foregroundStyle(.red)
-                        .frame(width: 200, height: 50)
-                        .offset(x:-100)
-                    ColorPicker("Choose Color", selection: $theme.themeColor)
-                        .bold()
-                        .foregroundStyle(.white)
-                        .font(.title)
-                        .offset(x:10)
-                }
-                    
             }
         }
     }
-}
+
