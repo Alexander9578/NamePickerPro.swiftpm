2c0c8fa44d507509ba878338a2615f3e5b6f19c9
nienio1 <admin@oskar-piatek.com>
2025-04-23 10:26:21 -0500

    added logo

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
             bundleIdentifier: "org.jhhs.628099.NamePickerPro",
             displayVersion: "1.0",
             bundleVersion: "1",
-            appIcon: .placeholder(icon: .cloud),
+            appIcon: .asset("AppIcon"),
             accentColor: .presetColor(.yellow),
             supportedDeviceFamilies: [
                 .pad,
