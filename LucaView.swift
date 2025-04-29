86cebefdb64230ced80fef058d13ce92fd3f55dc
Luca Fabian <lfabian8121@stu.d214.org>
2025-04-29 10:29:17 -0500

    color

--- LucaView.swift
+++ LucaView.swift
@@ -1,6 +1,7 @@
 import SwiftUI
 struct LucaView: View {
     @Binding var bgColor: Color
+    @State var textColor: Color = Color.white
     var body: some View {
         ZStack{
             bgColor
--- OskarView.swift
+++ OskarView.swift
@@ -1,6 +1,8 @@
 import SwiftUI
 struct OskarView: View {
+
     @Binding var bgColor: Color
+    @State var textColor: Color = Color.white
     
     var body: some View {
         ZStack{
