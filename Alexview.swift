f71873e5075918bc05a1a20ada635d84b578a557
Alexander9578 <adiaz8099@stu.d214.org>
2025-05-01 10:47:15 -0500

    Added picture to AlexView

--- Alexview.swift
+++ Alexview.swift
@@ -6,18 +6,42 @@ struct AlexView: View {
         ZStack{
             bgColor
                 .ignoresSafeArea()
-          
+            
             ZStack{
                 RoundedRectangle(cornerRadius: 10)
                     .foregroundStyle(.blue)
-                    .frame(width: 300, height: 50)
+                    .frame(width: 300, height: 50, alignment: .top)
+                   
                 
                 
                 Text("Alexander Diaz")
                     .font(.largeTitle)
                     .bold()
                     .foregroundStyle(textColor)
+             
+                 
             }
+            .frame(maxHeight: .infinity, alignment: .top)
+        
+            
+            ZStack{
+                
+                RoundedRectangle(cornerRadius: 10)
+                    .foregroundStyle(.blue)
+                    .frame(width: 320, height: 435, alignment: .top)
+                
+                Image("AlexPicture2")
+                    .resizable()
+                    .frame(width: 290, height: 400, alignment: .bottomLeading)
+                
+            }
+            
+           
+            
+            
+            
+          
+            
         }
     }
 }
