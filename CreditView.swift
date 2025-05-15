043db6109a4b307b9569371a7143b1b6fea85978
nienio1 <admin@oskar-piatek.com>
2025-05-15 11:07:37 -0500

    removed stuff

--- CreditView.swift
+++ CreditView.swift
@@ -24,72 +24,49 @@ struct CreditView: View {
                         .font(.largeTitle)
                         .offset(y:-10)
                 }
-                Button{
-                    
-                } label: {
-                    ZStack{
-                        RoundedRectangle(cornerRadius: 10)
-                            .foregroundStyle(tbColor)
-                            .frame(width: 200, height: 50);
-                        Text("Alex Diaz")
-                            .bold()
-                            .font(.title)
-                            .padding()
-                            .foregroundStyle(textColor)
-                    }
-                    
-                }
-                Button{
-                    
-                } label: {
-                    ZStack{
-                        RoundedRectangle(cornerRadius: 10)
-                            .foregroundStyle(tbColor)
-                            .frame(width: 200, height: 50);
-                        Text("Luca Fabian")
-                            .bold()
-                            .font(.title)
-                            .padding()
-                            .foregroundStyle(textColor)
-                    }
-                    
+                ZStack{
+                    RoundedRectangle(cornerRadius: 10)
+                        .foregroundStyle(tbColor)
+                        .frame(width: 200, height: 50);
+                    Text("Alex Diaz")
+                        .bold()
+                        .font(.title)
+                        .padding()
+                        .foregroundStyle(textColor)
                 }
                 
+                ZStack{
+                    RoundedRectangle(cornerRadius: 10)
+                        .foregroundStyle(tbColor)
+                        .frame(width: 200, height: 50);
+                    Text("Luca Fabian")
+                        .bold()
+                        .font(.title)
+                        .padding()
+                        .foregroundStyle(textColor)
+                }
+                ZStack{
+                    RoundedRectangle(cornerRadius: 10)
+                        .foregroundStyle(tbColor)
+                        .frame(width: 200, height: 50);
+                    Text("Oskar Piątek")
+                        .bold()
+                        .font(.title)
+                        .padding()
+                        .foregroundStyle(textColor)
+                }
+                ZStack{
+                    RoundedRectangle(cornerRadius: 10)
+                        .foregroundStyle(tbColor)
+                        .frame(width: 200, height: 50);
+                    Text("Ryusei Yamada")
+                        .bold()
+                        .font(.title)
+                        .padding()
+                        .foregroundStyle(textColor)
+                }
                 
-                Button{
-                    
-                } label: {
-                    ZStack{
-                        RoundedRectangle(cornerRadius: 10)
-                            .foregroundStyle(tbColor)
-                            .frame(width: 200, height: 50);
-                        Text("Oskar Piątek")
-                            .bold()
-                            .font(.title)
-                            .padding()
-                            .foregroundStyle(textColor)
-                    }
-                    
-                }
-                Button{
-                    
-                } label: {
-                    ZStack{
-                        RoundedRectangle(cornerRadius: 10)
-                            .foregroundStyle(tbColor)
-                            .frame(width: 200, height: 50);
-                        Text("Ryusei Yamada")
-                            .bold()
-                            .font(.title)
-                            .padding()
-                            .foregroundStyle(textColor)
-                    }
-                    
-                }
             }
-            
         }
     }
 }
-
-
